304b9592c9fa173a5ed3eca70dd6fd0b2b8c12ea
shtabyl <avnirog@gmail.com>
2025-04-27 13:10:57 +0300

    Perf: add screen interface

--- script.js
+++ script.js
@@ -76,8 +76,8 @@ function GameController(
     const getRoundsPlayed = () => roundsPlayed;
 
     const printNewRound = () => {
-        board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn.`);
+        // board.printBoard();
+        // console.log(`${getActivePlayer().name}'s turn.`);
     };
 
     const checkForWinner = (row, column) => {
@@ -90,7 +90,7 @@ function GameController(
             result.row += board.getBoard()[row][i].getValue();
             result.column += board.getBoard()[i][column].getValue();
         }
-        console.log(result);
+        // console.log(result); // log result calculations
         if (
             Object.values(result).includes(3) 
             || Object.values(result).includes(-3)
@@ -110,8 +110,10 @@ function GameController(
         }
 
         roundsPlayed++;
-        console.log(`Round ${roundsPlayed}`); // log round number
-        console.log(`Put ${getActivePlayer().name}'s mark into row ${row}, column ${column}`);
+        if (roundsPlayed <= 9) {
+            console.log(`Round ${roundsPlayed}`); // log round number
+        }
+        // console.log(`Put ${getActivePlayer().name}'s mark into row ${row}, column ${column}`);
         
         board.markCell(row, column, getActivePlayer().mark);
         
@@ -130,13 +132,13 @@ function GameController(
     }
 
     const startGame = () => {
-        printNewRound();
+        board.getBoard().forEach(row => row.forEach(cell => cell.addMark(0)));
         roundsPlayed++;
         console.log(`Round ${roundsPlayed}`); // log round number
+        printNewRound();
     }
     
     const stopGame = (winner) => {
-        // board.getBoard().forEach(row => row.forEach(cell => cell.addMark(0)));
         // board.getBoard().map(row => row.map(cell => cell.addMark(0)));
         roundsPlayed = 0;
         if (winner) {
@@ -151,7 +153,70 @@ function GameController(
     return { startGame, stopGame, playRound, getActivePlayer, switchPlayerTurn, getRoundsPlayed, checkForWinner, board };
 }
 
+function ScreenController() {
+    const game = GameController();
+    const playerTurnDiv = document.querySelector('.turn');
+    const boardDiv = document.querySelector('.board');
+    const startGameBtn = document.querySelector('.start-game');
+    const resultDiv = document.querySelector('.result');
+    const roundDiv = document.querySelector('.round');
 
-const game = GameController();
+    const updateScreen = () => {
+        // clear the board
+        boardDiv.textContent = '';
+
+        // get the newest version of the board and player turn
+        const activePlayer = game.getActivePlayer();
+        const board = game.board.getBoard();
+
+        // display player's turn, round, result
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+        roundDiv.textContent = `Round ${game.getRoundsPlayed()}`;
+
+        // render board squares
+        board.forEach((row, rowIndex) => {
+            row.forEach((cell, columnIndex) => {
+                const cellButton = document.createElement('button');
+                cellButton.classList.add('cell');
+                cellButton.dataset.row = rowIndex;
+                cellButton.dataset.column = columnIndex;
+                cellButton.textContent = cell.getValue();
+                boardDiv.appendChild(cellButton);
+            });
+        });
+    }
+
+    // Add event listener for the board
+    function clickHandleBoard(e) {
+        const selectedRow = e.target.dataset.row;
+        const selectedColumn = e.target.dataset.column;
+        // make sure cell was clicked not the gaps between
+        if (!selectedRow || !selectedColumn) {
+            return;
+        }
+
+        const result = game.playRound(selectedRow, selectedColumn);
+        if (result && result !== 'Tie!') {
+            resultDiv.textContent = `${result} wins!`;
+        } else if (result === 'Tie!') {
+            resultDiv.textContent = 'Tie!';
+        }
+        updateScreen();
+    }
+
+    boardDiv.addEventListener('click', clickHandleBoard);
+    
+    function clickStartGame() {
+        game.startGame();
+        roundDiv.textContent = `Round ${game.getRoundsPlayed()}`;
+        resultDiv.textContent = '';
+        updateScreen();
+    }
+    startGameBtn.addEventListener('click', clickStartGame);
+
+    updateScreen();
+}
+
+ScreenController();
 
 module.exports = { GameBoard, GameController };
